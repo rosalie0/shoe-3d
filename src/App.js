ced849e001b52ed617c5e08d5ddb741114ae852c
Rosalie Newman <rosalienewman0@gmail.com>
2022-12-26 14:32:52 -0500

    stuff installed, put shoe in public folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index";
+import { useRef, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+//useGLTF lets you import a 3d model
 
 function App() {
+  // const ref = useRef();
+  const [mainColor, setMainColor] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="card">
+          <div className="product-canvas">
+            {/* 3-D model goes here */}
+            <Canvas></Canvas>
+          </div>
+          <h2>Color chooser</h2>
+          <div className="colors">
+            <div>
+              <input type="color" id="head" name="head" value="#e66465" />
+              <label for="head">Main</label>
+            </div>
+
+            <div>
+              <input type="color" id="body" name="body" value="#f6b73c" />
+              <label for="body">Stripes</label>
+            </div>
+            <div>
+              <input type="color" id="body" name="body" value="#f6b73c" />
+              <label for="body">Soul</label>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
